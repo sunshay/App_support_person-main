2cacc6e3bfb311739775c4e901d0cea409394573
sunshay <sunshayina@outlook.com>
2022-12-07 10:11:21 -0800

    modify

--- app_support/models.py
+++ app_support/models.py
@@ -31,22 +31,22 @@ class UserManager(BaseUserManager):
         return user
 
 
-class Country(models.Model):
+class Email(models.Model):
     
-    country = models.CharField(max_length=20)
+    email = models.CharField(max_length=20)
 
     def __str__(self):
         return self.country
     
-class State(models.Model):
+class department(models.Model):
     
     state = models.CharField(max_length=20)
-    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    country = models.ForeignKey(department, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.state       
 
-class City(models.Model):
+class Phonenumber(models.Model):
 
     city = models.CharField(max_length=20)
     state = models.ForeignKey(State, on_delete=models.CASCADE)
